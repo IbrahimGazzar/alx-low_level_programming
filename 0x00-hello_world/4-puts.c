aaac0a91928af3dfeaffe27a3c1f1ba270939500
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-15 14:30:31 -0700

    Print with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  * main - Print a message with puts
+ *
+ * Return: Always return 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
