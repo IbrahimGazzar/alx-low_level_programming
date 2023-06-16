dbf5a4afc584095827289a988beda2592429a968
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-16 07:41:46 -0700

    Create a C file that finds the last digit and compares it to 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,11 +10,12 @@ int main(void)
 {
 	int n;
 	int last;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
-	printf("The last digit %d is ", n);
-       	if (last > 5)
+	printf("The last digit of %d is ", n);
+	if (last > 5)
 		printf("%d is greater than 5\n", last);
 	else if (last < 6)
 		printf("%d is less than 6 and not 0\n", last);
