5613bebf9a648cf67674f382c465d396e279b2a5
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-16 08:22:40 -0700

    Print the alphabet, in reverese

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print the entire lowercase alphabet
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
