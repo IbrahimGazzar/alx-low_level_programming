16760b2c648709678bf4b9dd97898bd37359392b
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-16 08:19:03 -0700

    Print the 10 digits, differently

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print all 10 digits
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
