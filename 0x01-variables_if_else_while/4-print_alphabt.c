18ab6d677b90d9e215b203a061bee1b97c552dc6
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-16 08:07:55 -0700

    Print the lphabet with exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print the entire lowercase alphabet except q and e
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if(char != 'e' && char != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
