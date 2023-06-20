2353a615db17ae9281cd2a66438d216e199c0ff3
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-20 11:38:47 -0700

    Create a C function that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two numbers and return the result
+ * @n: first number to be added
+ * @m: second number to be added
+ *
+ * Return: n + m
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+}
