702ecc1f50168608eeaabb473d4618000163ad90
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-20 06:54:16 -0700

    Print the entire alphabet in lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,16 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 void putputchar(void)
 {
 	putchar('_');
@@ -16,6 +29,6 @@ void print_alphabet(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-	putchar('\n');
+		_putchar(c);
+	_putchar('\n');
 }
