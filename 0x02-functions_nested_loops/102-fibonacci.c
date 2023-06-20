4ec15285fcaa67a25dc3ee5a8bab1f95cc7bac9d
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-20 15:00:38 -0700

    Create a C program that prints the first 50 fib numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a function that prints the first 50 numbers in the fibonacci series
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int fib1;
+	int fib2;
+	int temp;
+	int i;
+
+	fib1 = 0;
+	fib2 = 1;
+	for (i = 0; i < 50; i++)
+	{
+		temp = fib1;
+		fib1 = fib2;
+		fib2 = temp + fib1;
+		printf("%d, ", fib2);
+	}
+	putchar('\n');
+	return (0);
+}
