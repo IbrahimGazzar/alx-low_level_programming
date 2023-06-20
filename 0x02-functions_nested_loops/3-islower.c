22c3308c44dfef8446b628c0c8d7c700c7686e15
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-20 09:11:16 -0700

    Update documentation

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks if a character is lowercase or not
- * c: input character to be checked
+ * @c: input character to be checked
  * Return: 1 if input is lowercase, 0 otherwise
  */
 
