141a291beb2f037ea1961e3a0249e628232437eb
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-20 12:10:39 -0700

    Create a C function that adds two numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,27 +21,12 @@ void print_to_98(int n)
 		n_abs = n;
 	if (n > 98)
 	{
-		for (i = n; i > 98; i--)
-		{	
-			n_001 = n_abs % 10;
-			n_010 = ((n_abs % 100) - n_001) / 10;
-			n_100 = (n_abs - (n_abs % 100)) / 100;
-			if (i < 0)
-				_putchar('-');
-			if (n_100 > 0)
-				_putchar(n_100 + 48);
-			if (n_010 > 0)
-				_putchar(n_010 + 48);
-			_putchar(n_001);
-			_putchar(',');
-			_putchar(' ');
-
-		}
+		above_98(n);
 	}
 	else
 	{
 		for (i = n; i < 98; i++)
-		{	
+		{
 			n_001 = n_abs % 10;
 			n_010 = ((n_abs % 100) - n_001) / 10;
 			n_100 = (n_abs - (n_abs % 100)) / 100;
@@ -57,8 +42,36 @@ void print_to_98(int n)
 
 		}
 	}
-
 	_putchar('9');
 	_putchar('8');
-	_putchar('\n');		
+	_putchar('\n');
+}
+
+/**
+ * above_98 - called in case n is larger than 98
+ * @n: number that starts the sequence
+ *
+ * Return: no return value
+ */
+
+void above_98(int n)
+{
+	int i;
+	int n_100;
+	int n_010;
+	int n_001;
+
+	for (i = n; i > 98; i--)
+	{
+		n_001 = n_abs % 10;
+		n_010 = ((n_abs % 100) - n_001) / 10;
+		n_100 = (n_abs - (n_abs % 100)) / 100;
+		if (n_100 > 0)
+			_putchar(n_100 + 48);
+		if (n_010 > 0)
+			_putchar(n_010 + 48);
+		_putchar(n_001);
+		_putchar(',');
+		_putchar(' ');
+	}
 }
