d21631770d77f8f1a4819ce35c97c72a791bc8db
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-20 06:38:03 -0700

    Print '_putchar' with the use of a header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,3 +12,22 @@ int main(void)
 	_putchar();
 	return 0;
 }
+
+/**
+ * _putchar - print "_putchar"
+ *
+ * Return: no value
+ */
+
+int _putchar(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+}
