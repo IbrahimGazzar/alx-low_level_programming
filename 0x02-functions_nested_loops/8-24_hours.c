dbb145784d67bfdcac63f0818b61ec4470a73f7d
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-20 10:59:55 -0700

    Print all minutes of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print all minutes of the day in the HH:MM formate
+ *
+ * Return: no return value
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+	int hour_10;
+	int hour_01;
+	int minute_10;
+	int minute_01;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		hour_01 = hour % 10;
+		hour_10 = hour - hour_01;	
+		minute = 0;
+		for (minute = 0; minute < 60; minute++)
+		{
+			minute_01 = minute % 10;
+			minute_10 = minute - minute_01;
+			_putchar(hour_10);
+			_putchar(hour_01);
+			_putchar(':');
+			_putchar(minute_10);
+			_putchar(minute_01);
+			_putchar('\n');
+		}
+	}
+}
