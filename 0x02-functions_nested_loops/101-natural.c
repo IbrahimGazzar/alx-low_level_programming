21b743d004770b216a5f9f34a08b6d5d1ad089b1
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-20 14:42:13 -0700

    Create a C program that sums multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a function that computes the sum of multiples of 3 and 5 below 1024
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int sum;
+	int i3;
+	int i5;
+
+	sum = 0;
+	for (i3 = 3; i3 < 1024; i3+3)
+		sum += i3;
+	for (i5 = 5; i5 < 1024; i5+5)
+		sum += i5;
+	printf("%d\n", sum);
+	return (0);
+}
