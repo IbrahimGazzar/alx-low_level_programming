1c257ed74870bd9d36f9280c8adcfc39677058c3
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-22 09:27:50 -0700

    Create a C program that does the fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print numbers from 1 to 100, and FizzBuzz
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 != 0 && i % 5 !=0)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+			if(i % 5 == 0)
+				printf("Buzz");
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+}
