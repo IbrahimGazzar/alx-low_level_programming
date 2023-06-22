45c55b0389f314f321aa2801b205ec21f404278a
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-22 08:24:34 -0700

    Create a C function that prints diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line with given length n
+ * @n: length of the diagonal line
+ *
+ * Return: no return value
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
