15ceb25e46de57854fb33aa13fcdfaa5111d65b9
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-26 15:15:41 -0700

    Print only the even position characters of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints the first character then every 2 characters after that
+ * @str: String to be printed
+ *
+ * Return: no return value
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(s[i]);
+	}
+	_putchar('\n');
+}
