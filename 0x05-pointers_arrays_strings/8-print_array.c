54db048ee9ee06f8d0c40ca92da848bc206396b9
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-26 15:55:03 -0700

    Create a C function that prints the first n number of elements in array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * print_array - prints a given number of elements from a given array of integers
+ * print_array - prints a given number of elements for a given array
  * @a: array to be printed
  * @n: number of elements
  *
@@ -14,10 +14,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n < 1)
-		return;
-	printf("%d", a[0]);
-	for (i = 1; i < n; i++)
-		printf(", %d", a[i]);
+	if (n > 0)
+	{
+		printf("%d", a[0]);
+		for (i = 1; i < n; i++)
+			printf(", %d", a[i]);
+	}
 	putchar('\n');
 }
