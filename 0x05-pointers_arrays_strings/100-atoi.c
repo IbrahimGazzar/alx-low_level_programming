b6d3a312613b6cfe46327ae49ab94c4663878403
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-27 06:48:36 -0700

    Fix typo

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,14 +15,12 @@ int _atoi(char *s)
 
 	num = 0;
 	sign = 1;
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == '-')
+			sign *= -1;
 		if (s[i] >= '0' && s[i] <= '9')
-		{
-			if (s[i-1] == '-')
-				sign = -1;
 			num = (num * 10) + sign * (s[i] - 48);
-		}
 		if (num > 0 && (s[i] < '0' || s[i] > '9'))
 			break;
 	}
