7571337a66f307dccb1dabacdb62dda2508605ed
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-27 06:32:28 -0700

    Create a C function that finds the first sequence of numbers and makes an int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the integer
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int j;
+	int num;
+	int sign;
+
+	num = 0;
+	sign = 1;
+	for (i = 0; i != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			if (s[i-1] == '-')
+				sign = -1;
+			num = (num * 10) + sign * (s[i] - 48);
+		}
+	}
+	return (num);
+}
