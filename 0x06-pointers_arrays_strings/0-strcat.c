d6cbfaa01bd3691145d18d7bfd77b1c1949ff3ff
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-29 16:52:13 -0700

    Fix error in code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strcat - concatenates two strings
@@ -13,7 +12,7 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 	int destlen;
-	string newstr;
+	char *newstr;
 
 	destlen = 0;
 	for (i = 0; dest[i] != '\0'; i++)
