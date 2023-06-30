b99e934bf268ed0294e75a0f16409d081690a079
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-30 07:31:05 -0700

    .

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,15 +11,19 @@ void print_number(int n)
 {
 	int i;
 	int cnt;
+	int temp;
 
 	if (n < 0)
 		_putchar('-');
 	cnt = 0;
 	for (i = 1000000000; i >= 0; i /= 10)
 	{
-		if (n / i != 0 || cnt != 0)
+		temp = n / i;
+		if (temp != 0 || cnt != 0)
 		{
-			_putchar((n / i) + 48);
+			if (temp < 0)
+				temp = 0 - temp;
+			_putchar(temp + 48);
 			cnt++;
 		}
 	}
