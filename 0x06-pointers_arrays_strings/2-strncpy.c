8c06733e35e3112adfdb926a4b198c5aefd6efc7
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-29 17:57:48 -0700

    Create a C function that copies a string with a limit of n bytes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy one string into another, with a limit of n bytes
+ * @dest: pointer to the string to be copied into
+ * @src: the source of the string
+ * @n: byte limit
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int cnt;
+
+	cnt = 0;
+	while (*src != '\0' && cnt < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		cnt++;
+	}
+	return (*dest);
+}
