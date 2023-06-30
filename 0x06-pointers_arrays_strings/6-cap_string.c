408b126f1f422ae9b17924ccb569dba1ff52f99d
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-29 19:49:32 -0700

    .

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize the first letter in each string
+ * @str: string to be edited
+ *
+ * Return: the edited string
+ */
+
+char *cap_string(char *str)
+{
+	char *newstr;
+	int i;
+	int ip;
+
+	newstr = str;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if(str[i] >= 'a' && str[i] <= 'z' && i > 0)
+		{
+			ip = i - 1;
+			if (str[ip] == ' ' || str[ip] == '\t' || str[ip] == '\n' || str[ip] == ',')
+				str[i] -= 32;
+			else if (str[ip] == ';' || str[ip] == '.' || str[ip] == '!')
+				str[i] -= 32;
+			else if (str[ip] == '"' || str[ip] == '(' || str[ip] == ')')
+				str[i] -= 32;
+			else if (str[ip] == '}'|| str[ip] == '{' || str[ip] == '?')
+				str[i] -= 32;
+		}
+	}
+	return (newstr);
+}
