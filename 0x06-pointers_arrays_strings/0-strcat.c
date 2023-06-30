f0568b094afd10572918d4be98b4708baa25b602
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-29 17:37:01 -0700

    Create a C function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,22 +12,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int k;
-	int len;
 	char *newstr;
 
-	len = 0;
-	for (i = 0; dest[i] != '\0'; i++)
-		len++;
-	for (i = 0; src[i] != '\0'; i++)
-		len++;
-	len++;
-	k = 0;
-	newstr = (char*)malloc(len * sizeof(char));
-	for (i = 0; dest[i] != '\0'; i++, k++)
-		newstr[k] = dest[i];
-	for (i = 0; src[i] != '\0'; i++, k++)
-		newstr[k] = src[i];
+	newstr = dest;
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
 	return (newstr);
 }
