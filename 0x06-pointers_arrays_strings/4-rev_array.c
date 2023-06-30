8b0a9ccd60911da6b1932fc8371ca78a69b3003d
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-29 19:10:51 -0700

    Create a C function that reverses an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the contents of an array of integers
+ * @a: array to be reversed
+ * @n: number of elements in the array
+ *
+ * Return: no return value
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	for (i = 0; i < n; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = a[i];
+	}
+}
