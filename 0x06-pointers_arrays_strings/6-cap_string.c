9f7c6af2b7ed84da28058566746d85e74dc00c78
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-29 19:59:24 -0700

    .

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,13 +21,15 @@ char *cap_string(char *str)
 		if (str[i] >= 'a' && str[i] <= 'z' && i > 0)
 		{
 			ip = i - 1;
-			if (str[ip] == ' ' || str[ip] == '\t' || str[ip] == '\n' || str[ip] == ',')
+			if (str[ip] == ' ' || str[ip] == '\t' || str[ip] == '\n')
 				str[i] -= 32;
 			else if (str[ip] == ';' || str[ip] == '.' || str[ip] == '!')
 				str[i] -= 32;
 			else if (str[ip] == '"' || str[ip] == '(' || str[ip] == ')')
 				str[i] -= 32;
 			else if (str[ip] == '}'|| str[ip] == '{' || str[ip] == '?')
+				str[i] -= 32;
+			else if (str[ip] == ',')
 				str[i] -= 32;
 		}
 	}
