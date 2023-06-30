dfd871607a87276928c65cb6616761570982f2d2
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-06-30 07:14:55 -0700

    Fix calculations

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -19,10 +19,10 @@ char *rot13(char *str)
 		if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
 		{
 			ch = *str +
-			13 * ((*str >= 'A' && *str < 'N') || (*str >= 'a' && *str < 'n')) -
-			13 * (*str > 'M' && *str <= 'Z') || (*str > 'm' && *str <= 'z');
+			(13 * ((*str >= 'A' && *str < 'N') || (*str >= 'a' && *str < 'n'))) -
+			(13 * ((*str > 'M' && *str <= 'Z') || (*str > 'm' && *str <= 'z')));
 			*str = ch;
-		} 
+		}
 		str++;
 	}
 	return (newstr);
