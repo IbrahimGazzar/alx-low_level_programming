4cb2ebc3fc82ff72b05f419cbc7bc70b16b3d8af
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-03 13:29:40 -0700

    Create a C function that calculates the sum of diagonals of a 2d array

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagsums - print the sums of diagonals of given 2d integer array
+ * @a: given 2d array
+ * @size: size of the array
+ *
+ * Return: no return value
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int leftdiag;
+	int rightdiag;
+
+	leftdiag = 0;
+	rightdiag = 0;
+	for (i = 0; i < size; i++)
+	{
+		leftdiag += a[i][i];
+		rightdiag += a[i][size - 1 - i];
+	}
+	printf("%d, %d\n", leftdiag, rightdiag);
+}
