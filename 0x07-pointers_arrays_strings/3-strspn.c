4976d03da34c29c11f894cdd5eaa561b6ee33897
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-03 09:44:31 -0700

    Create a C function that searches for a group of characters in a string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - checks if characters from a string exists in another
+ * @s: string to be searched
+ * @accept: string whose characters are searched
+ *
+ * Return: number of occurences of accept characters in s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	char *ss;
+	int occ;
+
+	occ = 0;
+	while (*accept != '\0')
+	{
+		ss = s;
+		while (*ss != '\0')
+		{
+			if (*ss == *accept)
+				occ++;
+			ss++;
+		}
+		accept++;
+	}
+	return (occ);
+}
