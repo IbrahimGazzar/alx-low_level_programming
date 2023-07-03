3bed1cdcbfe21919d8f1544ff28c61720d3977dc
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-03 08:41:13 -0700

    Create a C function that sets a character in a specfied memory area

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory pointed to by s with b
+ * @s: pointer pointing at a memory area
+ * @b: character to fill the area
+ * @n: the first bytes of the memory area s
+ *
+ * Return: pointer to memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *news;
+
+	news = s;
+	while (n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+	return (news);	
+}
