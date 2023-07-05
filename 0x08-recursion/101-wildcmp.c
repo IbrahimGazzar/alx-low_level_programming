4b27f295ef9a9e0fd96ca2d8f9e36077581ea2b5
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-05 12:28:29 -0700

    .

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,8 +12,6 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '\0')
 		return (*s1 == '0');
-	if (*s1 == *s2 && *s1 != '\0')
-		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '\0')
@@ -22,5 +20,7 @@ int wildcmp(char *s1, char *s2)
 			return (1);
 		return (wildcmp(s1, s2 + 1));
 	}
+	if (*s1 == *s2 && *s1 != '\0')
+		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
 }
