bf315b971c96e1c99dab4b7abdc9fad34c642e7c
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-05 12:14:32 -0700

    .

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -21,3 +21,20 @@ int wildcmp(char *s1, char *s2)
 	}
 	return (0);
 }
+/**
+ * astreisk_tst - called for special cases of *
+ * @s1: pointer of the first string
+ * @s2: pointer of the second string
+ *
+ * Return: 1 if s1 and s2 are identical, 0 otherwise
+ */
+int astreisk_tst(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+		return (wildcmp(s1, s2 + 1));
+	if (wildcmp(s1, s2 + 1))
+		return (1);
+	else
+		return (astreisk_tst(++s1, s2));
+	return (0);
+}
