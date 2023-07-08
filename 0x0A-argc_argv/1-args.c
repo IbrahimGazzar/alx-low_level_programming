cd8accc950b7d9360975a2feab3b520cdceba841
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-07 17:34:17 -0700

    Write a program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * foo - dummy function for unused variables
+ * @bar: the unused variable
+ *
+ * Return: no return value
+ */
+
+void foo(char *bar[])
+{
+	(void)bar;
+}
+
+/**
+ * main - a function that prints the name of the file
+ * @argc: number of arguments for main
+ * @argv: an array of strings of the given arguements
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	foo(argv);
+	return (0);
+}
