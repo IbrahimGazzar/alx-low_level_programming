7a5129c7ccda4b8509a5684532e8661deb787875
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-07 18:23:03 -0700

    Create a C function that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a function that adds positive numbers
+ * @argc: number of arguements
+ * @argv: sent arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
