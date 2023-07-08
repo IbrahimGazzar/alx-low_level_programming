ab2c9503cd2b72a7e2c70df47785d0f778bc9d16
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-07 17:20:45 -0700

    Create a C file that prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a function that prints the name of the file
+ * @argc: number of arguments for main
+ * @argv: an array of strings of the given arguements
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
