4b75174afd0856600d8852ce8a9bb7e9c6b37c76
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-07 17:25:40 -0700

    Use unused variables

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void foo(int bar);
+
 /**
  * main - a function that prints the name of the file
  * @argc: number of arguments for main
@@ -11,6 +13,16 @@
 
 int main(int argc, char *argv[])
 {
+	foo(argc);
 	printf("%s\n", argv[0]);
 	return (0);
+}
+/**
+ * foo - dummy function to use unused variables
+ * @bar: variable to be dummied
+ *
+ * Return: no return value
+ */
+void foo(int bar) {
+    (void)bar;
 }
