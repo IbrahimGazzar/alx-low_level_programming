d3347546f3366b65f32b19e082c88d2fd60388bf
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-07 18:09:19 -0700

    Add a condition for error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,25 +2,17 @@
 #include <stdlib.h>
 
 /**
- * foo - dummy function for unused variables
- * @bar: the unused variable
- *
- * Return: no return value
- */
-
-void foo(int bar)
-{
-	(void)bar;
-}
-
-/**
  * main - a function that multiplies two numbers
  * @argc: number of arguements
  * @argv: sent arguments
+ *
+ * Return: always 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	if (argc < 3)
+		printf("Error");
 	printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
 	foo(argc);
 	return (0);
