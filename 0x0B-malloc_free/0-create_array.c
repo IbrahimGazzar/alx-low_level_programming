142103f772844173dab03e9ef8e0a48c1418d2ae
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-10 11:33:47 -0700

    Intialize file, create readme file, and create a C function that creates a variable size array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * create_array - a function that creates an array of character and intializes it with a char
+ * @size: size of the array to be created
+ * @c: character to intialize the array with
+ *
+ * Return: the array created
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	arr = malloc(sizeof(int) * size);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	return (arr);
+}
