9f0e96a4277448b6c30aff5e22732f4fda4b25ae
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-11 09:05:55 -0700

    Create a C function that concatentes program arguements into a single string

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,39 +12,24 @@
 
 char *argstostr(int ac, char **av)
 {
-	char **str;
 	int i;
-	int j;
 	char *newstr;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	str = malloc(sizeof(char *) * (ac - 1));
+	newstr = av[1];
 	for (i = 1; i < ac; i++)
 	{
-		str[i - 1] = malloc(sizeof(char) * strlen(av[i]));
-		if (str[i - 1] == NULL)
-		{
-			for (j = 0; j < i - 1; j++)
-				free(str[j]);
-			free(str);
-			return (NULL);
-		}
-		str[i - 1] = av[i];
-	}
-	newstr = str[0];
-	for (i = 0; i < (ac - 1); i++)
-	{
-		if (i > 0)
+		if (i > 1)
 		{
 			*newstr = '\n';
 			newstr++;
 		}
-		while (*str[i] != '\0')
+		while (*av[i] != '\0')
 		{
-			*newstr = *str[i];
+			*newstr = *av[i];
 			newstr++;
-			str[i]++;
+			av[i]++;
 		}
 	}
 	*newstr = '\n';
