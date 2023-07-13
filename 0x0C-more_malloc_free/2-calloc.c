c9cfbcb14a6881fa948faf656ba1ec72e1f435a1
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-13 09:36:56 -0700

    Intialize array elements

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,12 +11,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *arrptr;
+	char *arrptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arrptr = malloc(size * nmemb);
 	if (arrptr == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		arrptr[nmemb] = 0;
 	return (arrptr);
 }
