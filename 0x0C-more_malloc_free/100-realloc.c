526a48a87530a6addefc2cfe147925c19c9658c4
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-13 12:39:03 -0700

    Create a C function that reallocates memory of a pointer

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
+#include <string.h>
 
 /**
  * _realloc - reallocates the memory pointed to by a pointer
@@ -22,7 +23,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	newptr = malloc(new_size);
-	if (new_ptr == NULL)
+	if (newptr == NULL)
 		return (NULL);
 	if (ptr == NULL && new_size > 0)
 		return (newptr);
