6b465aeac6c792db9c72eaf674152254ec5d03e7
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-17 15:03:45 -0700

    Create a C function that creates a variable of type dog_t

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - creates a new variable of type dog_t
+ * @name: input for the name field
+ * @age: input for the age field
+ * @owner: input for the owner field
+ *
+ * Return: the new variable of type dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggo;
+	char *dog_name;
+	char *dog_owner;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	doggo = malloc(sizeof(float) + strlen(name) + strlen(owner) + 2);
+	if (doggo == NULL)
+		return (NULL);
+	dog_name = name;
+	dog_owner = owner;
+	doggo->name = strcpy(dog_name, name);
+	doggo->age = age;
+	doggo->owner = strcpy(dog_owner, owner);
+	return (doggo);
+}
