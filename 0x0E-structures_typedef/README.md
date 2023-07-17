db43e20310288a440f19f10d644bc1eb8eb83e93
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-17 12:30:02 -0700

    Create a C header that contains definition of struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This folder contains C files that make use of the struct and typedef features
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - contains dog information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner:
+}
