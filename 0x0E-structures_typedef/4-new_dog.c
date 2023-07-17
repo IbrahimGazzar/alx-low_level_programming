26f13de027da95f5071a556a72318c681c12fcfd
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-17 15:42:18 -0700

    .

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,7 +20,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	doggo = malloc(sizeof(float) + strlen(name) + strlen(owner) + 2);
+	doggo = malloc(sizeof(dog_t));
 	if (doggo == NULL)
 		return (NULL);
 	dog_name = name;
