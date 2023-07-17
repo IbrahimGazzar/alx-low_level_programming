991d99dd3b8750a964d7d974d326811c24a5ea1b
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-17 15:50:10 -0700

    free the dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_dog - frees a variable of type dog_t
+ * @d: pointer at the variable to be freed
+ *
+ * Return: no return value
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
