d4fee2ef167c8910f39277d98719d0461c715470
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-17 12:51:49 -0700

    .

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
 /**
  * struct dog - contains dog information
  * @name: name of the dog
  * @age: age of the dog
  * @owner: the dog's owner
+ *
+ * Description: This structure defines dog information
  */
 
 struct dog
