da73890bf2599332ec27fb6cbd099d39cc5c5a36
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-20 12:45:03 -0700

    Create a C function that sums any number of given arguements

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums up all its parameters
+ * @n: number of parameters
+ * @...: parameters to be summed
+ * 
+ * Return: sum of the given parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list argues;
+	unsigned int i;
+	int sum;
+
+	sum = 0;
+	if (n > 0)
+	{
+		va_start (argues, n);
+		for (i = 0; i < n; i++)
+			sum += va_arg (argues, int);
+	}
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This folder contains various C variadic functions
