2803610ef25b274cfc8aa250cce4164b0dfb7d93
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-20 15:26:11 -0700

    Add a helper function for formatting

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include "variadic_functions.h"
 
 /**
  * print_all - prints given variables in given formats
@@ -16,37 +17,61 @@ void print_all(const char * const format, ...)
 	va_list argues;
 	int i;
 	char *ss;
+	char *clean;
+	int j;
 
+	j = 0;
 	i = 0;
-	va_start(argues, format);
+	clean = malloc(sizeof(char) * (strlen(format) + 1));
 	while (format[i] != '\0')
 	{
-		switch (format[i])
+		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+			clean[j++] = format[i];
+		i++;
+	}
+	clean[j] = '\0';
+	i = 0;
+	va_start(argues, format);
+	while (clean[i] != '\0')
+	{
+		switch(clean[i])
 		{
 			case 'c':
-				printf("%c", va_arg(argues, int));
+				printf("%c%s", va_arg(argues, int), (const char*)print_help(clean, i));
 				break;
 			case 'i':
-				printf("%d", va_arg(argues, int));
+				printf("%d%s", va_arg(argues, int), (const char*)print_help(clean, i));
 				break;
 			case 'f':
-				printf("%f", va_arg(argues, double));
+				printf("%f%s", va_arg(argues, double), (const char*)print_help(clean, i));
 				break;
 			case 's':
 				ss = va_arg(argues, char*);
 				if (ss == NULL)
 				{
-					printf("(nil)");
+					printf("(nil)%s", (const char*)print_help(clean, i));
 					break;
 				}
-				printf("%s", ss);
-				break;
-			default:
+				printf("%s%s", ss, (const char*)print_help(clean, i));
 				break;
 		}
-		if ((size_t)i < strlen(format) - 1)
-			printf(", ");
 		i++;
 	}
 	putchar('\n');
+}
+/**
+ * print_help - helps by formatting the text to be printed
+ * @format: format string
+ * @i: current position in the string
+ *
+ * Return: ", " if i isn't the last element, empty string otherwise
+ */
+char *print_help(const char * const format, int i)
+{
+	char *help;
+
+	help = "";
+	if ((size_t)i < strlen(format) - 1)
+		help = ", ";
+	return (help);
 }
