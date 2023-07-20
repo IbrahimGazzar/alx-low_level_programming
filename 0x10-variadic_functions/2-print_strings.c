6f87dd82c67da3aafe910337ebdf2eaa2427c97e
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-20 13:09:40 -0700

    Create a C function that prints given strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - print given strings
+ * @separator: separator between strings
+ * @n: number of strings to be printed
+ * @...: strings to be printed
+ *
+ * Return: no return value
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list argues;
+	unsigned int i;
+	char *ss;
+
+	if (n < 1)
+	{
+		putchar('\n');
+		return;
+	}
+	va_start(argues, n);
+	for (i = 0; i < n; i++)
+	{
+		ss = va_arg(argues, int);
+		if (ss != NULL)
+			printf("%s", ss);
+		else
+			printf("(nil)");
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
