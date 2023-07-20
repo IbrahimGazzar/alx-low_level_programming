eb45ca8b5c0d9dc9dd1e3c67ab279c19df138ca1
IbrahimGazzar <ibrahim.m.elgazzar@gmail.com>
2023-07-20 15:44:27 -0700

    Create helper functions for formatting

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,41 +18,31 @@ void print_all(const char * const format, ...)
 	int i;
 	char *ss;
 	char *clean;
-	int j;
 
-	j = 0;
-	i = 0;
-	clean = malloc(sizeof(char) * (strlen(format) + 1));
-	while (format[i] != '\0')
-	{
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-			clean[j++] = format[i];
-		i++;
-	}
-	clean[j] = '\0';
+	clean = clean_format(format);
 	i = 0;
 	va_start(argues, format);
 	while (clean[i] != '\0')
 	{
-		switch(clean[i])
+		switch (clean[i])
 		{
 			case 'c':
-				printf("%c%s", va_arg(argues, int), (const char*)print_help(clean, i));
+				printf("%c%s", va_arg(argues, int), print_help(clean, i));
 				break;
 			case 'i':
-				printf("%d%s", va_arg(argues, int), (const char*)print_help(clean, i));
+				printf("%d%s", va_arg(argues, int), print_help(clean, i));
 				break;
 			case 'f':
-				printf("%f%s", va_arg(argues, double), (const char*)print_help(clean, i));
+				printf("%f%s", va_arg(argues, double), print_help(clean, i));
 				break;
 			case 's':
 				ss = va_arg(argues, char*);
 				if (ss == NULL)
 				{
-					printf("(nil)%s", (const char*)print_help(clean, i));
+					printf("(nil)%s", print_help(clean, i));
 					break;
 				}
-				printf("%s%s", ss, (const char*)print_help(clean, i));
+				printf("%s%s", ss, print_help(clean, i));
 				break;
 		}
 		i++;
@@ -74,4 +64,30 @@ char *print_help(const char * const format, int i)
 	if ((size_t)i < strlen(format) - 1)
 		help = ", ";
 	return (help);
+}
+/**
+ * clean_format - clean a given format from unused characters
+ * @format: format to be cleaned
+ *
+ * Return: format after cleaning
+ */
+char *clean_format(const char * const format)
+{
+	char *clean;
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	clean = malloc(sizeof(char) * (strlen(format) + 1));
+	while (format[i] != '\0')
+	{
+		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f')
+			clean[j++] = format[i];
+		else if (format[i] == 's')
+			clean[j++] = format[i];
+		i++;
+	}
+	clean[j] = '\0';
+	return (clean);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,4 +7,6 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 char *print_help(const char * const format, int i);
+char *clean_format(const char * const format);
+
 #endif
