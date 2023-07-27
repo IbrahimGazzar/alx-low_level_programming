f94ff3683654a213227c3ada4c8f491b6b398e6b
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-07-27 15:52:56 -0700

    Create a C function that adds a node to a given linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node to a linked list
+ * @head: address of start of the list
+ * @str: string to be put in the node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr;
+	list_t *node;
+
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+	node->str = strdup(str);
+	node->len = strlen(str);
+	node->next = NULL;
+	if (*head == NULL)
+		*head = node;
+	else
+	{
+		ptr = *head;
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		ptr->next = node;
+	}
+	return (node);
+}
