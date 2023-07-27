c43745dc6b4b129743b79ccf6a881e133b81429d
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-07-27 15:06:02 -0700

    Fix formatting for code and output

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -23,9 +23,9 @@ size_t print_list(const list_t *h)
 	while (ptr != NULL)
 	{
 		if (ptr->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s", ptr->len, ptr->str);
+			printf("[%d] %s\n", ptr->len, ptr->str);
 		num++;
 		ptr = ptr->next;
 	}
