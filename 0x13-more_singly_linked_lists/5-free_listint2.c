882857184eab102a7bd49efafac0e0874c41d8c9
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-07-31 09:53:14 -0700

    Create an alternative C function for freeing a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list and sets the head to NULL
+ * @head: address of the start of the function
+ *
+ * Return: no return value
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
+	}
+}
