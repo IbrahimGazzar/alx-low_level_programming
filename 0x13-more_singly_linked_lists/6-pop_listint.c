b1d5787b827f0fc1aa2102bc6c9b74cb8c2e0399
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-07-31 13:59:12 -0700

    Create a C function that pops the head of the list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a linked list
+ * @head: pointer to the linked list
+ *
+ * Return: head node's data, 0 if empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int num;
+
+	if (head == NULL)
+		return (0);
+	num = (*head)->n;
+	ptr = (*head)->next;
+	free(*head);
+	*head = ptr;
+	return (num);
+}
