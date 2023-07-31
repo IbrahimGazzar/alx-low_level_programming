99539f06119a3253d13447c2f0a02879f6f413c9
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-07-31 15:02:39 -0700

    Create a C function that inserts an element in a given index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts new node at a given position
+ * @head: list to be inserted in
+ * @idx: index of the new node's position
+ * @n: int content of the new node
+ * Return: address of the new node, NULL if failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ptr;
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	ptr = *head;
+	while (idx > 1)
+	{
+		ptr = ptr->next;
+		if (ptr == NULL)
+			return (NULL);
+		idx--;
+	}
+	new->next = ptr->next;
+	ptr->next = new;
+	return (new);
+}
