af7e208961c11f5c4988e4775993c97caf44c876
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-07-31 14:20:59 -0700

    Create a C function that finds a list member with a given index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - finds a node by its index
+ * @head: pointer to node to start counting from
+ * @index: n-th node after head
+ *
+ * Return: node of the given index, NULL if non-existent
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr;
+
+	if (head == NULL || index < 0)
+		return (NULL);
+	ptr = head;
+	while (index > 0)
+	{
+		ptr = ptr->next;
+		if (ptr == NULL)
+			return (NULL);
+		index--;
+	}
+	return (ptr);
+}
