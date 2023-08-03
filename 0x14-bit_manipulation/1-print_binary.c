d436bb02111d354bdd9495dbe6d607011c96d3e4
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-08-03 08:22:16 -0700

    Create a C function that prints the binary representation of an unsigned number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a uint
+ * @n: unsigned int number
+ *
+ * Return: no return value
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+
+	i = 1;
+	while (i < n)
+		i *= 2;
+	if (i > n)
+		i = i >> 1;
+	while (i > 0)
+	{
+		if ((n & i) > 0)
+		{
+			n -= i;
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		i = i >> 1;
+	}
+}
