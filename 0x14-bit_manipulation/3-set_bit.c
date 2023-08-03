e1716b016b74ae47afcd029cec63d60cfa2722d6
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-08-03 09:40:53 -0700

    Create a C function that sets the value of a bit in a number

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * set_bit - sets a bit to 1
+ * @n: pointer to the number to be manipulated
+ * @index: index of the bit to be manipulated
+ *
+ * Return: 1 if successful, -1 otherwise
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int i;
+
+	i = 1;
+	while (index)
+	{
+		i <<= 1;
+		index--;
+	}
+	if (*n & i)
+		return (-1);
+	else
+		*n += i;
+	return (1);
+}
