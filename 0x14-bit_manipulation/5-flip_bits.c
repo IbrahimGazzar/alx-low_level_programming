cf7b87f161e7236db1565a53ebb6b7c590235ba7
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-08-03 10:09:14 -0700

    Create a C function that checks the bit difference between two ints

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * flip_bits - finds the required bits to be flipped for two ints to be equal
+ * @n: first integer
+ * @m: second integer
+ *
+ * Return: number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i;
+	int bits;
+
+	bits = 0;
+	i = 1;
+	while (i < n || i < m)
+		i <<= 1;
+	if (i > n && i > m)
+		i >>= 1;
+	while (i > 0)
+	{
+		if ((m & i) != (n & i))
+			bits++;
+		i >>= 1;
+	}
+	return (bits);
+}
