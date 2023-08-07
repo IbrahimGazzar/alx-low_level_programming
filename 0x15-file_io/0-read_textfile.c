87daf41ce980ed6af9c2cb133303a8524aa858a8
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-08-07 11:11:34 -0700

    Edit code to only use allowed syscalls

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,30 +14,33 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
-	char c;
+	int fd;
+	char *c;
 	ssize_t count;
+	ssize_t read_bytes;
 
 	if (filename == NULL)
 		return (0);
-	fp = fopen(filename, "r");
-	count = 0;
-	while ((c = fgetc(fp)) != EOF && letters)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	c = malloc(letters * sizeof(char));
+	if (c == NULL)
 	{
-		_putchar(c);
-		letters--;
-		count++;
+		close(fd);
+		return (0);
 	}
+	read_bytes = read(fd, c, letters);
+	if (read_bytes == -1)
+	{
+		free(c);
+		close(fd);
+		return (0);
+	}
+	count = write(STDOUT_FILENO, c, read_bytes);
+	free(c);
+	close(fd);
+	if (count != read_bytes)
+		return (0);
 	return (count);
-}
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
