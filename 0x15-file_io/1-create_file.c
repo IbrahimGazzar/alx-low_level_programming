ced6646269aba068d11c549d69062a997c246404
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-08-07 11:57:38 -0700

    Create a C function that creates a new file, or truncates an existing one

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to be created
+ * @text_content: contents to be put in the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_write;
+	int len;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+	{
+		if (errno != EEXIST)
+			return (-1);
+		close(fd);
+		len = truncate(filename, 0);
+		if (len == -1)
+			return (-1);
+		fd = open(filename, O_WRONLY);
+	}
+	len = strlen(text_content);
+	bytes_write = write(fd, text_content, len);
+	close (fd);
+	if (bytes_write != len)
+		return (-1);
+	return (1);
+}
