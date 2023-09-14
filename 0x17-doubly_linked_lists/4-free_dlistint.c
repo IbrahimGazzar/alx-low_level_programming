a99db12d52c3c13ee28a774ee0707f32acc33703
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-09-14 08:34:14 -0700

    Create a C function that frees a doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: start of the list to be freed
+ *
+ * Return: no return value
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr = head->next;
+
+	while (ptr != NULL)
+	{
+		free(head);
+		head = ptr;
+		ptr = ptr->next;
+	}
+	free(head);
+}
