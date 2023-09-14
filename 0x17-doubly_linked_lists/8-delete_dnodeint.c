e9a9b51e3df7867951959cd7b7d0ac65a8700fd3
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-09-14 14:44:06 -0700

    Create a C function that deletes nodes in a doubly linked list, specified by their indicies

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete node at a given index
+ * @head: list containing the node to be deleted
+ * @index: index of the node to be deleted
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *ptr;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	ptr = *head;
+	if (index == 0)
+	{
+		if ((*head)->next != NULL)
+			((*head)->next)->prev = NULL;
+		*head = (*head)->next;
+		return (1);
+	}
+	while (ptr != NULL)
+	{
+		if (i == index)
+		{
+			if (ptr->prev != NULL)
+				(ptr->prev)->next = ptr->next;
+			if (ptr->next != NULL)
+				(ptr->next)->prev = ptr->prev;
+			return (1);
+		}
+		i++;
+		ptr = ptr->next;
+	}
+	return (-1);
+}
