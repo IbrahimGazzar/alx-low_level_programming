8126908160bcb9ba6d408283e67cfa1f88155b67
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-10-05 14:34:54 -0700

    Finally fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,12 +14,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *node = malloc(sizeof(hash_node_t)), *temp;
 
-	if (node == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
+	if (node == NULL)
 		return (0);
-	node->key = malloc(sizeof(char) * (strlen(key) + 1));
-	node->value = malloc(sizeof(char) * (strlen(value) + 1));
-	strcpy(node->key, key);
-	strcpy(node->value, value);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (node->key == NULL || node->value == NULL)
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
+		return (0);
+	}
 	node->next = NULL;
 	if (ht->array[index] == NULL)
 		ht->array[index] = node;
@@ -28,9 +33,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		temp = ht->array[index];
 		while (temp != NULL)
 		{
-			if (strcmp(temp->key, key) == 0)
+			if(strcmp(temp->key, key) == 0)
 			{
-				strcpy(ht->array[index]->value, value);
+				free(node->key);
+				free(node->value);
+				free(node);
+				free(temp->value);
+				temp->value = strdup(value);
 				return (1);
 			}
 			temp = temp->next;
